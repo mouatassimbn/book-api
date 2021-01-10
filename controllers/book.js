978c40144e26f6d33b9a2e1b883470d7a741c708
mouatassim boulahsen <m.ouatassim89@gmail.com>
2021-01-10 16:53:55 -0400

    Created controller methods to handle book crud operations

---
+++ controllers/book.js
@@ -0,0 +1,9 @@
+exports.index = (req, res, nxt) => {};
+
+exports.show = (req, res, nxt) => {};
+
+exports.store = (req, res, nxt) => {};
+
+exports.update = (req, res, nxt) => {};
+
+exports.destroy = (req, res, nxt) => {};
