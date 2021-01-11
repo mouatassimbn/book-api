a536ce51859cf0073d3efc7cd9794e692d048909
mouatassim boulahsen <m.ouatassim89@gmail.com>
2021-01-11 09:17:41 -0400

    Implemented a method to find book by id

--- models/book.js
+++ models/book.js
@@ -2,11 +2,12 @@ const mongodb = require("mongodb");
 const getDb = require("../util/database").getDb;
 
 class Book {
-  constructor(isbn, title, author, description) {
+  constructor(isbn, title, author, description, id) {
     this.isbn = isbn;
     this.title = title;
     this.author = author;
     this.description = description;
+    this._id = id;
   }
 
   save() {
@@ -27,7 +28,13 @@ class Book {
       });
   }
 
-  static findById() {}
+  static findById(bookId) {
+    const db = getDb();
+
+    return db
+      .collection("books")
+      .findOne({ _id: new mongodb.ObjectId(bookId) });
+  }
 
   static find() {}
 }
