15c20ac881e332e2d9ec4f8a89252822316c3bee
mouatassim boulahsen <m.ouatassim89@gmail.com>
2021-01-11 09:48:24 -0400

    Store method is implemented

--- controllers/book.js
+++ controllers/book.js
@@ -1,4 +1,13 @@
-exports.index = (req, res, nxt) => {};
+const Book = require("../models/book");
+const Book = require("../models/book");
+
+exports.index = (req, res, nxt) => {
+  Book.fetchAll()
+    .then((books) => {
+        res.json(books);
+    })
+    .catch((err) => console.log(err));
+};
 
 exports.show = (req, res, nxt) => {};
 
