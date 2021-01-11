66d7177e80a5c46e37178fb940c5e41040219433
mouatassim boulahsen <m.ouatassim89@gmail.com>
2021-01-11 09:54:15 -0400

    Show method is implemented

--- controllers/book.js
+++ controllers/book.js
@@ -4,12 +4,20 @@ const Book = require("../models/book");
 exports.index = (req, res, nxt) => {
   Book.fetchAll()
     .then((books) => {
-        res.json(books);
+      res.json(books);
     })
     .catch((err) => console.log(err));
 };
 
-exports.show = (req, res, nxt) => {};
+exports.show = (req, res, nxt) => {
+  const bookId = req.params.id;
+
+  const book = Book.findById(bookId)
+    .then((book) => {
+      res.json(book);
+    })
+    .catch((err) => console.log(err));
+};
 
 exports.store = (req, res, nxt) => {};
 
