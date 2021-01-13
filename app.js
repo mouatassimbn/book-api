80eacde3324135202c1b44ae46f615d1a5cf401b
mouatassim boulahsen <m.ouatassim89@gmail.com>
2021-01-13 15:53:24 -0400

    Installed and initialized mongoose
    Deleted database util class since there's no more use

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const path = require("path");
+const mongoose = require("mongoose");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -14,7 +14,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api", apiRoutes);
 
 const PORT = process.env.PORT || 3000;
+const CONNECTIONSTRING = process.env.CONNECTIONSTRING || null;
 
-mongoConnect(() => {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-});
+mongoose
+  .connect(CONNECTIONSTRING)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.log(err));
