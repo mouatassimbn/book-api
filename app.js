bc961b226e434aa71ba62673701a339454f94d80
mouatassim boulahsen <m.ouatassim89@gmail.com>
2021-01-18 23:02:26 -0400

    removed mongodb deprecation messages

--- app.js
+++ app.js
@@ -16,10 +16,10 @@ const PORT = process.env.PORT || 3000;
 const CONNECTIONSTRING = process.env.CONNECTIONSTRING || null;
 
 // Fix deprication warnings
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true)
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 
 mongoose
   .connect(CONNECTIONSTRING)
